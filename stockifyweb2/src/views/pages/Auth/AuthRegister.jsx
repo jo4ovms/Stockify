d586cb6257dd850166e7536e0518c3e558b0857c
jo4ovms <joaovictormacields@gmail.com>
2024-10-27 20:05:57 -0300

    feat: enhance authentication page with detailed content, layout and validation

--- stockifyweb2/src/views/pages/Auth/AuthRegister.jsx
+++ stockifyweb2/src/views/pages/Auth/AuthRegister.jsx
@@ -1,27 +1,31 @@
 import { Box, Typography, Button, Stack } from "@mui/material";
-import { useState } from "react";
+import { Formik, Form, Field } from "formik";
 import { useNavigate, Link } from "react-router-dom";
+import * as Yup from "yup";
 import CustomTextField from "../../../components/forms/theme-elements/CustomTextField.jsx";
 import AuthService from "../../../services/AuthService";
 
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "O nome de usuário deve ter pelo menos 3 caracteres")
+    .required("Nome de usuário é obrigatório"),
+  email: Yup.string()
+    .email("Endereço de email inválido")
+    .required("Email é obrigatório"),
+  password: Yup.string()
+    .min(8, "A senha deve ter pelo menos 8 caracteres")
+    .max(40, "A senha deve ter no maximo 40 caracteres")
+    .required("Senha é obrigatória"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "As senhas devem ser iguais")
+    .required("Confirmação de senha é obrigatória"),
+});
+
 const AuthRegister = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [message, setMessage] = useState("");
   const navigate = useNavigate();
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-    setMessage("");
-
-    if (password !== confirmPassword) {
-      setMessage("As senhas devem ser iguais");
-      return;
-    }
-
-    AuthService.register(username, email, password).then(
+  const handleRegister = (values, { setSubmitting, setStatus }) => {
+    AuthService.register(values.username, values.email, values.password).then(
       () => {
         navigate("/auth/login");
       },
@@ -32,7 +36,8 @@ const AuthRegister = () => {
             error.response.data.message) ||
           error.message ||
           error.toString();
-        setMessage(resMessage);
+        setStatus({ message: resMessage });
+        setSubmitting(false);
       }
     );
   };
@@ -42,78 +47,117 @@ const AuthRegister = () => {
       <Typography fontWeight="700" variant="h2" mb={1}>
         Registrar
       </Typography>
-      <Stack spacing={1}>
-        <Box>
-          <Typography variant="subtitle1" fontWeight={600} mb="5px">
-            Nome de Usuário
-          </Typography>
-          <CustomTextField
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-        <Box mt="20px">
-          <Typography variant="subtitle1" fontWeight={600} mb="5px">
-            Email
-          </Typography>
-          <CustomTextField
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-        <Box mt="20px">
-          <Typography variant="subtitle1" fontWeight={600} mb="5px">
-            Senha
-          </Typography>
-          <CustomTextField
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-        <Box mt="20px" mb="10px">
-          <Typography variant="subtitle1" fontWeight={600} mb="5px">
-            Confirme a Senha
-          </Typography>
-          <CustomTextField
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-      </Stack>
-      <Box mt={2}>
-        <Button
-          color="primary"
-          variant="contained"
-          size="large"
-          fullWidth
-          onClick={handleRegister}
-        >
-          Registrar
-        </Button>
-      </Box>
-      {message && (
-        <Typography color="error" mt={2}>
-          {message}
-        </Typography>
-      )}
+
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleRegister}
+      >
+        {({ isSubmitting, errors, touched, status }) => (
+          <Form>
+            <Stack spacing={1}>
+              <Box>
+                <Typography variant="subtitle1" fontWeight={600} mb="5px">
+                  Nome de Usuário
+                </Typography>
+                <Field
+                  name="username"
+                  as={CustomTextField}
+                  variant="outlined"
+                  fullWidth
+                  error={touched.username && Boolean(errors.username)}
+                  helperText={touched.username && errors.username}
+                />
+              </Box>
+
+              <Box mt="20px">
+                <Typography variant="subtitle1" fontWeight={600} mb="5px">
+                  Email
+                </Typography>
+                <Field
+                  name="email"
+                  type="email"
+                  as={CustomTextField}
+                  variant="outlined"
+                  fullWidth
+                  error={touched.email && Boolean(errors.email)}
+                  helperText={touched.email && errors.email}
+                />
+              </Box>
+
+              <Box mt="20px">
+                <Typography variant="subtitle1" fontWeight={600} mb="5px">
+                  Senha
+                </Typography>
+                <Field
+                  name="password"
+                  type="password"
+                  as={CustomTextField}
+                  variant="outlined"
+                  fullWidth
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password}
+                />
+              </Box>
+
+              <Box mt="20px" mb="10px">
+                <Typography variant="subtitle1" fontWeight={600} mb="5px">
+                  Confirme a Senha
+                </Typography>
+                <Field
+                  name="confirmPassword"
+                  type="password"
+                  as={CustomTextField}
+                  variant="outlined"
+                  fullWidth
+                  error={
+                    touched.confirmPassword && Boolean(errors.confirmPassword)
+                  }
+                  helperText={touched.confirmPassword && errors.confirmPassword}
+                />
+              </Box>
+            </Stack>
+
+            <Box mt={2}>
+              <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                fullWidth
+                type="submit"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Carregando..." : "Registrar"}
+              </Button>
+            </Box>
+
+            {status && status.message && (
+              <Typography color="error" mt={2} align="center">
+                {status.message}
+              </Typography>
+            )}
+          </Form>
+        )}
+      </Formik>
+
       <Typography
         variant="subtitle1"
         textAlign="center"
         color="textSecondary"
         mt={2}
       >
-        Já possui uma conta? <Link to="/auth/login">Entrar em uma conta</Link>
+        Já possui uma conta?{" "}
+        <Link
+          to="/auth/login"
+          style={{ textDecoration: "none", color: "#5D87FF" }}
+        >
+          Entrar em uma conta
+        </Link>
       </Typography>
     </Box>
   );
--- stockifyweb2/src/views/pages/Auth/Login.jsx
+++ stockifyweb2/src/views/pages/Auth/Login.jsx
@@ -10,46 +10,29 @@ const Login = () => {
       <Box
         sx={{
           position: "relative",
-          "&:before": {
-            content: '""',
-            background: "radial-gradient(#d2f1df, #d3d7fa, #bad8f4)",
-            backgroundSize: "400% 400%",
-            animation: "gradient 15s ease infinite",
-            position: "absolute",
-            height: "100%",
-            width: "100%",
-            opacity: "0.3",
-          },
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "radial-gradient(#d2f1df, #d3d7fa, #bad8f4)",
+          animation: "gradient 15s ease infinite",
         }}
       >
-        <Grid
-          container
-          spacing={0}
-          justifyContent="center"
-          sx={{ height: "100vh" }}
+        <Card
+          elevation={7}
+          sx={{
+            padding: 4,
+            maxWidth: 400,
+            width: "100%",
+            zIndex: 1,
+            position: "relative",
+          }}
         >
-          <Grid
-            size={{ xs: 12, sm: 12, lg: 4, xl: 3 }}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Card
-              elevation={7}
-              sx={{ p: 4, zIndex: 1, width: "100%", maxWidth: "500px" }}
-            >
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                sx={{ width: "600px", height: "100px", mb: 2, ml: -12 }}
-              >
-                <Logo />
-              </Box>
-              <AuthLogin />
-            </Card>
-          </Grid>
-        </Grid>
+          <Box display="flex" justifyContent="center" mb={3}>
+            <Logo />
+          </Box>
+          <AuthLogin />
+        </Card>
       </Box>
     </PageContainer>
   );
