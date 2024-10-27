5cb2b02e8b27aa1d3b0051b545e8b748bbad8288
jo4ovms <joaovictormacields@gmail.com>
2024-10-27 15:58:46 -0300

    fix: suppliers can be find without accents

--- src/main/java/com/jo4ovms/StockifyAPI/specification/SupplierSpecification.java
+++ src/main/java/com/jo4ovms/StockifyAPI/specification/SupplierSpecification.java
@@ -1,12 +1,24 @@
 package com.jo4ovms.StockifyAPI.specification;
 
 import com.jo4ovms.StockifyAPI.model.Supplier;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
+
 
 public class SupplierSpecification {
     public static Specification<Supplier> hasName(String name) {
-        return (root, query, criteriaBuilder) ->
-                name == null ? null : criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%");
+        return (Root<Supplier> root, jakarta.persistence.criteria.CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
+            if (name == null || name.trim().isEmpty()) {
+                return criteriaBuilder.conjunction();
+            }
+
+            String normalizedSearchTerm = name.toLowerCase();
+            return criteriaBuilder.like(
+                    criteriaBuilder.function("unaccent", String.class, criteriaBuilder.lower(root.get("name"))),
+                    "%" + normalizedSearchTerm + "%"
+            );
+        };
     }
 
     public static Specification<Supplier> hasProductType(String productType) {
