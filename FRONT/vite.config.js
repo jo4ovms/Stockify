421547c2ae34ffbd4a3be13c6e081312b7aa7749
jo4ovms <joaovictormacields@gmail.com>
2024-10-28 12:53:56 -0300

    chore: add text compression on vite for better perfomance

--- FRONT/vite.config.js
+++ FRONT/vite.config.js
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 // eslint-disable-next-line import/namespace
 import { defineConfig } from "vite";
+import compression from "vite-plugin-compression";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), compression({ algorithm: "brotliCompress" })],
   server: {
     historyApiFallback: true,
   },
