2668407f7a2f825c7274a984a3637545d5df1069
jo4ovms <joaovictormacields@gmail.com>
2024-11-02 11:28:48 -0300

    feat: implement EntityGraph to optimize entity fetching in queries and changed cascadeType on Product to remove stock when deleted

--- API/src/main/java/com/jo4ovms/StockifyAPI/model/Product.java
+++ API/src/main/java/com/jo4ovms/StockifyAPI/model/Product.java
@@ -44,7 +44,7 @@ public class Product {
     @JoinColumn(name = "supplier_id", nullable = false)
     private Supplier supplier;
 
-    @OneToOne(mappedBy = "product", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToOne(mappedBy = "product", cascade = CascadeType.REMOVE)
     private Stock stock;
 
     @CreatedDate
--- API/src/main/java/com/jo4ovms/StockifyAPI/repository/UserRepository.java
+++ API/src/main/java/com/jo4ovms/StockifyAPI/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.jo4ovms.StockifyAPI.repository;
 
 import com.jo4ovms.StockifyAPI.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByUsername(String username);
 
     Boolean existsByUsername(String username);
