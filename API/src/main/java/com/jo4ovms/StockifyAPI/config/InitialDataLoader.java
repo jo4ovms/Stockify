11d553b734ffc968076392b54105e2c6efdd6f22
jo4ovms <joaovictormacields@gmail.com>
2024-11-03 16:49:39 -0300

    feat: add unaccent extension to PostgreSQL on startup

--- API/src/main/java/com/jo4ovms/StockifyAPI/config/InitialDataLoader.java
+++ API/src/main/java/com/jo4ovms/StockifyAPI/config/InitialDataLoader.java
@@ -6,6 +6,7 @@ import com.jo4ovms.StockifyAPI.model.Role;
 import com.jo4ovms.StockifyAPI.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -18,11 +19,22 @@ public class InitialDataLoader implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     private static final Logger logger = Logger.getLogger(InitialDataLoader.class.getName());
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        try {
+            jdbcTemplate.execute("CREATE EXTENSION IF NOT EXISTS unaccent;");
+            logger.info("Extensão unaccent ativada no banco de dados.");
+        } catch (Exception e) {
+            logger.warning("Erro ao ativar a extensão unaccent: " + e.getMessage());
+        }
+
         logger.info("Checking roles in database...");
 
         if(roleRepository.count() == 0) {
