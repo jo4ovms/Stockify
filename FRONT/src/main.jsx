95614a58c4b882c9a490fb89154bacf900f53ca0
jo4ovms <joaovictormacields@gmail.com>
2024-11-03 16:18:05 -0300

    refactor:  local database postgresql

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -1,27 +1,27 @@
 import { CircularProgress } from "@mui/material";
-import { StrictMode, Suspense } from "react";
+import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Suspense
-      fallback={
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "300px",
-          }}
-        >
-          <CircularProgress />
-        </div>
-      }
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Suspense>
-  </StrictMode>
+  // <StrictMode>
+  <Suspense
+    fallback={
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "300px",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    }
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Suspense>
+  //</StrictMode>
 );
